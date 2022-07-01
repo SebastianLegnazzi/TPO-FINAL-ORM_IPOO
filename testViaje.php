5dacdb8bd68e4f6b4762e41e22bec93597149c32
Sebastian <sebastian.legnazzi@est.fi.uncoma.edu.ar>
2022-07-01 10:50:20 -0300

    Corrijo Código Redundante

--- testViaje.php
+++ testViaje.php
@@ -125,17 +125,13 @@ function viajeRepetido($objViajeIngresado){
 function pedirEmpresa(){
     $objEmpresa = new Empresa();
     $stringEmpresa = stringObjEmpresa();
-    echo "Ingrese el codigo de alguna de las siguientes empresas, en caso de no estar, ingrese 0 para cargar una: "."\n".$stringEmpresa;
-    $empresaElegida = trim(fgets(STDIN));
-    $empresaElegida = verificadorInt($empresaElegida);
-    if($empresaElegida == 0){
-        crearEmpresa();
-        $resp = $objEmpresa->buscar(count($objEmpresa->listar("")));
-    }else{
-        $resp = $objEmpresa->buscar($empresaElegida);
-    }
-    while(!$resp){
-        echo "El codigo de la empresa no existe, porfavor ingrese alguna de las siguientes opciones o ingrese 0 para cargar una: "."\n".$stringEmpresa;
+    $resp = true;
+    do{
+        if($resp){
+            echo "Ingrese el codigo de alguna de las siguientes empresas, en caso de no estar, ingrese 0 para cargar una: "."\n".$stringEmpresa;
+        }else{
+            echo "El codigo de la empresa no existe, porfavor ingrese alguna de las siguientes opciones o ingrese 0 para cargar una: "."\n".$stringEmpresa;
+        }
         $empresaElegida = trim(fgets(STDIN));
         $empresaElegida = verificadorInt($empresaElegida);
         if($empresaElegida == 0){
@@ -144,7 +140,7 @@ function pedirEmpresa(){
         }else{
             $resp = $objEmpresa->buscar($empresaElegida);
         }
-    }
+    }while(!$resp);
     return $objEmpresa;
 }
 
@@ -155,17 +151,13 @@ function pedirEmpresa(){
 function pedirResponsable(){
     $objResponsable = new ResponsableV();
     $stringResponsable = stringObjResponsable();
-    echo "Ingrese el numero de empleado de alguna de los siguientes resposnables, en caso de no estar, ingrese 0 para cargar uno: "."\n".$stringResponsable;
-    $responsableElegido = trim(fgets(STDIN));
-    $responsableElegido = verificadorInt($responsableElegido);
-    if($responsableElegido == 0){
-        crearResponsable();
-        $resp = $objResponsable->buscar(count($objResponsable->listar("")));
-    }else{
-        $resp = $objResponsable->buscar($responsableElegido);
-    }
-    while(!$resp){
-        echo "El numero de empleado no existe, porfavor ingrese uno de los siguientes responsables o ingrese 0 para cargar una: "."\n".$stringResponsable;
+    $resp = true;
+    do{
+        if($resp){
+            echo "Ingrese el numero de empleado de alguna de los siguientes resposnables, en caso de no estar, ingrese 0 para cargar uno: "."\n".$stringResponsable;
+        }else{
+            echo "El numero de empleado no existe, porfavor ingrese uno de los siguientes responsables o ingrese 0 para cargar una: "."\n".$stringResponsable;
+        }
         $responsableElegido = trim(fgets(STDIN));
         $responsableElegido = verificadorInt($responsableElegido);
         if($responsableElegido == 0){
@@ -174,7 +166,7 @@ function pedirResponsable(){
         }else{
             $resp = $objResponsable->buscar($responsableElegido);
         }
-    }
+    }while(!$resp);
     return $objResponsable;
 }
 
@@ -904,6 +896,121 @@ function opcionesViaje(){
     } while ($opcion != 0);
 }
 
+function opcionesInicio($seleccion){
+    switch($seleccion){
+            
+        // Salir del programa
+        case 0:
+            exit();
+        break;
+
+        // Cargar un viaje
+        case 1:
+            separador();
+            echo "Ingrese la cantidad de viajes que desea agregar: ";
+            $cant = trim(fgets(STDIN));
+            $cant = verificadorInt($cant);
+            creaViajes($cant);
+        break;
+
+        // Cargar una empresa
+        case 2:
+            crearEmpresa();
+        break;
+
+        // Cargar una responsable
+        case 3:
+            crearResponsable();
+        break;
+
+        // Modificar datos de una empresa
+        case 4:
+            $stringEmpresa = stringObjEmpresa();
+            $objEmpresa = new Empresa();
+            echo "Ingrese la identificacion de la empresa que desea modificar: "."\n".$stringEmpresa;
+            $empresaModificar = trim(fgets(STDIN));
+            $resp = $objEmpresa->buscar($empresaModificar);
+            if($resp){
+                cambiarDatoEmpresa($objEmpresa);
+            }else{
+                separador();
+                echo "El numero de la empresa seleccionada no existe!"."\n";
+                separador();
+            }
+        break;
+
+        // Modificar datos de un responsable
+        case 5:
+            $stringResponsable = stringObjResponsable();
+            $objResponsable = new ResponsableV();
+            echo "Ingrese el numero de empleado del responsable que desea modificar: "."\n".$stringResponsable;
+            $responsableModificar = trim(fgets(STDIN));
+            $resp = $objResponsable->buscar($responsableModificar);
+            if($resp){
+                cambiarDatoResponsable($objResponsable);
+            }else{
+                separador();
+                echo "El numero del responsable seleccionado no existe!"."\n";
+                separador();
+            }
+        break;
+
+        //Modificar un viaje
+        case 6:
+            opcionesViaje();
+        break;
+
+        // Mostrar todos los viajes
+        case 7:
+            separador();
+            mostrarViajes();
+            separador();
+        break;
+        
+        // Elimina un viaje
+        case 8:
+            separador();
+            echo "Los viajes son: "."\n".stringObjViajes();
+            echo "Ingrese el codigo del viaje que desea eliminar: ";
+            $objViaje = new Viaje();
+            $codigo = trim(fgets(STDIN));
+            $codigo = verificadorInt($codigo);
+            $resp = $objViaje->buscar($codigo);
+            while(!$resp){
+                echo "El codigo ingresado no existe, porfavor ingrese uno de los viajes mostrados"."\n".stringObjViajes();
+                $codigo = trim(fgets(STDIN));
+                $resp = $objViaje->buscar($codigo);
+            }
+            $objViaje->obtenerPasajeros();
+            if(count($objViaje->getArrayObjPasajero()) == 0){
+                $resp = $objViaje->eliminar();
+                if($resp){
+                    echo "El viaje fue eliminado correctamente!"."\n";
+                }else{
+                    echo "el codigo ingresado no coicide con ningun viaje!"."\n";
+                }
+            }else{
+                separador();
+                echo "El viaje no se puede eliminar porque tiene pasajeros!"."\n";
+            }
+            separador();
+        break;
+
+        default : 
+        echo "El número que ingresó no es válido, por favor ingrese un número del 0 al 8"."\n"."\n";
+        break;
+    }
+}
+
+function noHayViajes($opcion){
+    $opcion = verificadorInt($opcion);
+    while(($opcion < 0) || ($opcion > 3)){
+        echo "No hay viajes! porfavor ingrese las opciones 0, 1, 2 o 3: ";
+        $opcion = trim(fgets(STDIN));
+    }
+    return $opcion;
+}
+
 /**************************************/
 /********* PROGRAMA PRINCIPAL *********/
 /**************************************/
@@ -913,153 +1020,16 @@ do{
     $objViaje = new Viaje();
     $dimensionViaje = count($objViaje->listar(""));
     $seleccion = "";
-    if($dimensionViaje > 0){
+    if($dimensionViaje == 0){
+        echo "No hay viajes! solamente podra ingresar la opcion 0, 1, 2 o 3: ";
         $seleccion = menuInicio();
-        switch($seleccion){
-            
-            // Salir del programa
-            case 0:
-                exit();
-            break;
-
-            // Cargar un viaje
-            case 1:
-                separador();
-                echo "Ingrese la cantidad de viajes que desea agregar: ";
-                $cant = trim(fgets(STDIN));
-                $cant = verificadorInt($cant);
-                creaViajes($cant);
-            break;
-    
-            // Cargar una empresa
-            case 2:
-                crearEmpresa();
-            break;
-
-            // Cargar una responsable
-            case 3:
-                crearResponsable();
-            break;
-
-            // Modificar datos de una empresa
-            case 4:
-                $stringEmpresa = stringObjEmpresa();
-                $objEmpresa = new Empresa();
-                echo "Ingrese la identificacion de la empresa que desea modificar: "."\n".$stringEmpresa;
-                $empresaModificar = trim(fgets(STDIN));
-                $resp = $objEmpresa->buscar($empresaModificar);
-                if($resp){
-                    cambiarDatoEmpresa($objEmpresa);
-                }else{
-                    separador();
-                    echo "El numero de la empresa seleccionada no existe!"."\n";
-                    separador();
-                }
-            break;
-
-            // Modificar datos de un responsable
-            case 5:
-                $stringResponsable = stringObjResponsable();
-                $objResponsable = new ResponsableV();
-                echo "Ingrese el numero de empleado del responsable que desea modificar: "."\n".$stringResponsable;
-                $responsableModificar = trim(fgets(STDIN));
-                $resp = $objResponsable->buscar($responsableModificar);
-                if($resp){
-                    cambiarDatoResponsable($objResponsable);
-                }else{
-                    separador();
-                    echo "El numero del responsable seleccionado no existe!"."\n";
-                    separador();
-                }
-            break;
-
-            //Modificar un viaje
-            case 6:
-                opcionesViaje();
-            break;
-    
-            // Mostrar todos los viajes
-            case 7:
-                separador();
-                mostrarViajes();
-                separador();
-            break;
-            
-            // Elimina un viaje
-            case 8:
-                separador();
-                echo "Los viajes son: "."\n".stringObjViajes();
-                echo "Ingrese el codigo del viaje que desea eliminar: ";
-                $objViaje = new Viaje();
-                $codigo = trim(fgets(STDIN));
-                $codigo = verificadorInt($codigo);
-                $resp = $objViaje->buscar($codigo);
-                while(!$resp){
-                    echo "El codigo ingresado no existe, porfavor ingrese uno de los viajes mostrados"."\n".stringObjViajes();
-                    $codigo = trim(fgets(STDIN));
-                    $resp = $objViaje->buscar($codigo);
-                }
-                $objViaje->obtenerPasajeros();
-                if(count($objViaje->getArrayObjPasajero()) == 0){
-                    $resp = $objViaje->eliminar();
-                    if($resp){
-                        echo "El viaje fue eliminado correctamente!"."\n";
-                    }else{
-                        echo "el codigo ingresado no coicide con ningun viaje!"."\n";
-                    }
-                }else{
-                    separador();
-                    echo "El viaje no se puede eliminar porque tiene pasajeros!"."\n";
-                }
-                separador();
-            break;
-    
-            default : 
-            echo "El número que ingresó no es válido, por favor ingrese un número del 0 al 8"."\n"."\n";
-            break;
-        }
+        $seleccion = noHayViajes($seleccion);
+        opcionesInicio($seleccion);
     }else{
-        do{
-        separador();
-        echo "No hay viajes ingresados todavia! ingrese alguna de las siguentes opciones"."\n".
-             "0- Salir"."\n".
-             "1- Cargar una empresa"."\n".
-             "2- Cargar un responsable"."\n".
-             "3- Cargar un viaje"."\n";
-        $opcion = trim(fgets(STDIN));
-            switch($opcion){
-                
-                // Salir del programa
-                case 0:
-                    exit();
-                break;
-
-                // Cargar una empresa
-                case 1:
-                    crearEmpresa();
-                break;
-        
-                // Cargar un responsable
-                case 2:
-                    crearResponsable();
-                break;
-        
-                // Cargar un viaje
-                case 3:
-                    separador();
-                    echo "Ingrese la cantidad de viajes que desea agregar: ";
-                    $cant = trim(fgets(STDIN));
-                    $cant = verificadorInt($cant);
-                    creaViajes($cant);
-                break;
-        
-                default : 
-                echo "El número que ingresó no es válido, por favor ingrese un número del 0 al 3"."\n"."\n";
-                break;
-            }
-        $dimensionViaje = count($objViaje->listar(""));
-        }while(($opcion != 0) && ($dimensionViaje == 0));
+        $seleccion = menuInicio();
+        opcionesInicio($seleccion);
     }
 }while($seleccion != 0);
+
 
 ?>
